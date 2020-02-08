b0e65bce9d3be98e29578d7eb12feec5c739e41d
jona <jona@localhost.localdomain>
2020-02-07 21:21:37 -0500

    second database migration for user model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username=models.CharField(max_length=200)
+    email=models.CharField(max_length=225)
+    password=models.TextField()
+
+    def __str__(self):
+        return self.username
