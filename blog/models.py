611e7ca92d2c2a8bcc41ff646e76473e4b5026c0
jona <jona@localhost.localdomain>
2020-02-07 21:32:21 -0500

    second model of posts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class User(models.Model):
     username=models.CharField(max_length=200)
@@ -8,3 +8,13 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    date_posted=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
