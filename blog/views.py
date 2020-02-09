b7582a3fadf8fb164800eb7cf0a1f66a348f09af
jona <jona@localhost.localdomain>
2020-02-09 01:31:49 -0500

    inherited base template and retrieved database posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post,User
 
 # Create your views here.
 
 def home(request):
-    return render(request,'blog/index.html')
+
+    posts=Post.objects.all()
+
+    context={
+    'posts':posts
+    }
+
+    return render(request,'blog/index.html',context)
