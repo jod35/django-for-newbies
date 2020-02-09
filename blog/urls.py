4152828edecc5776564e45201409aa8cf3ce5d21
jona <jona@localhost.localdomain>
2020-02-09 01:46:21 -0500

    created a page for posting new blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+  path('new/',views.create_post,name='blog_new'),
   path('',views.home,name='blog_home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,8 @@ def home(request):
     }
 
     return render(request,'blog/index.html',context)
+
+
+def create_post(request):
+    context={}
+    return render(request,'blog/new_post.html',context)
