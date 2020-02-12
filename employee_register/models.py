f0c5a879840d7535a01859c42dc65ec76a523231
jona <jona@localhost.localdomain>
2020-02-12 01:37:51 -0500

    database for employee's

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    title=models.CharField(max_length=50)
+
+
+
+class Employee(models.Model):
+    full_name=models.CharField(max_length=200)
+    emp_code=models.CharField(max_length=3)
+    email=models.CharField(max_length=20)
+    position=models.ForeignKey(Position,on_delete=models.CASCADE)
+    
