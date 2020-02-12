e1acfb4c1b28354760524ca01ee3042647345cfd
jona <jona@localhost.localdomain>
2020-02-12 02:04:52 -0500

    views for the employee app

---
+++ employee_register/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.employee_list,name='emloyee_list'),
+    path('add/'views.employee_form,name='employee_form'),
+    path('delete/'views.employee_delete,name='employee_delete')
+]
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def employee_list(request):
+    return 
+
+def employee_form(request):
+    return
+
+def emaployee_delete(request):
+    return
